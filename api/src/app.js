5fff310dd450ec6cb5154b2f1b6bd320c790937f
fspiritosi <fspiritosi@gmail.com>
2023-06-08 11:16:42 -0300

    arreglando la petición del back

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-dogs-deploy-o0di5nvjl-fspiritosi.vercel.app"
+    "https://pi-dogs-deploy-self.vercel.app/"
   ); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
